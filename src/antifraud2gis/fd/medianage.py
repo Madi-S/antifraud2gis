1a345582242f836bbc565e471de659c8d8294140
Yaroslav Polyakov <yaroslaff@gmail.com>
2025-04-15 02:23:23 +0700

    better explain for MA

--- src/antifraud2gis/fd/medianage.py
+++ src/antifraud2gis/fd/medianage.py
@@ -21,7 +21,7 @@ class MedianAgeFD(BaseFD):
         super().__init__(c, explain=explain)
         self.user_ages = list()
         self.records = list()
-
+        self.low_rating = 0
 
     def feed(self, cr: Review, empty: bool = False):
 
@@ -34,15 +34,21 @@ class MedianAgeFD(BaseFD):
             user_age = (cr.created - u.birthday()).days
             self.user_ages.append(user_age)
             self.records.append(f"{u.public_id} ({u.name} {cr.created_str} - {u.birthday_str}) = {user_age}")
+        else:
+            self.low_rating += 1
 
 
     def get_score(self):
 
+        if len(self.user_ages) == 0:
+            return self.score
+        
         median_age = int(np.median(self.user_ages))
 
         self.score['median_user_age'] = median_age
         
         if len(self.user_ages) > settings.median_user_age_nusers and self.score['median_user_age'] <= settings.median_user_age:
+            self.score['ma_low_rating'] = self.low_rating
             self.score['detections'].append(f"median_user_age {self.score['median_user_age']} <= {settings.median_user_age} ({sum(a <= settings.median_user_age for a in self.user_ages)} of {len(self.user_ages)})")
     
         return self.score
@@ -50,9 +56,10 @@ class MedianAgeFD(BaseFD):
     def explain(self, fh):
         print(f"Explanation for median_user_age", file=fh)
 
-        for line in self.records:
-            print(line, file=fh)
+        for idx, line in enumerate(self.records, start=1):
+            print(f'{idx} {line}', file=fh)
 
-        print(f"ages ({self.score['median_user_age']}): {sorted(self.user_ages)}", file=fh)
-        print(f"result: {self.score['median_user_age']}", file=fh)
+        print(f"reviews with low rating: {self.score['ma_low_rating']}", file=fh)
+        print(f"ages ({len(self.user_ages)}): {sorted(self.user_ages)}", file=fh)
+        print(f"median age: {self.score['median_user_age']}", file=fh)
         print("", file=fh)
