a17ec4d0a467e0e84dae613fc04752300756b1fe
Yaroslav Polyakov <yaroslaff@gmail.com>
2025-04-19 14:23:53 +0700

    af2worker

--- src/antifraud2gis/cli/af2worker.py
+++ src/antifraud2gis/cli/af2worker.py
@@ -2,13 +2,16 @@ import dramatiq
 import sys
 from ..tasks import fraud_task 
 from dramatiq.cli import main as dramatiq_main
-
+from ..logger import logger, loginit
 
 """
 af2worker antifraud2gis.tasks
 """
 def main():
     sys.argv = ["dramatiq", "-p1", "-t1", "antifraud2gis.tasks"]
+    loglevel = "DEBUG" # (or "INFO")
+    loginit(loglevel)
+    logger.debug(f"Starting dramatiq worker ({loglevel})")
     dramatiq_main()
 
 if __name__ == "__main__":
